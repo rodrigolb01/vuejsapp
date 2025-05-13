9904d5dd76494dff64fbb7584e585bbcb3d261ac
Kuroe <rodrigolb01@gmail.com>
2025-05-13 15:55:01 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ export default {
   data() {
     return {
       name: "John Doe",
-      status: "pendin"
+      status: "inactive",
+      tasks: ["cleaning", "coding", "cooking", "going to class"],
+      link: "https://www.aliexpress.com/"
     };
   }
 }
@@ -14,6 +16,15 @@ export default {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is Inactive</p>
+
+  <h3>Tasks</h3>
+  <ul v-for="task in tasks" :key="task">
+    <h1>{{task}}</h1>
+  </ul>
+  <!-- <a v-bind:href="link">Click Me</a > -->
+   <a :href="link">Click Me!</a>
+
+   <button v-on:click="toggleStatus">Change Status</button>
 </template>
 
 
