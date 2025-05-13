7a74df6f73c9158dfeea606903b0ece7d4fd71d7
Kuroe <rodrigolb01@gmail.com>
2025-05-13 01:29:52 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "pendin"
+    };
+  }
+}
+</script>
+
+<template>
+  <h1>Hello {{name}}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
+</template>
+
+
